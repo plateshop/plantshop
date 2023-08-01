6a305d99c0d06753de16f58443ac03b307858396
[IoT1] 최혜진 <rhkdeo97@gmail.com>
2023-08-01 22:03:40 +0900

    20230801 회의 전 커밋

--- src/data.tsx
+++ src/data.tsx
@@ -25,7 +25,31 @@ let data = [
         img: "https://crowcanyon.co.kr/web/product/medium/202208/0b7b9fd6b2ac3d7551b06f217895106d.jpg",
         title : "D11 머그 핑크마블",
         price : "￦20,000"
-      }
+      },
+
+      {
+        id : 4,
+        img: "https://crowcanyon.co.kr/web/product/medium/202301/c52d4c8ab79814b7c9faf3e8cb54f1e0.jpg",
+        title : "C93 텀블러 민트 히비스커스",
+        price : "￦24,000"
+      },
+
+      {
+        id : 5,
+        img: "https://crowcanyon.co.kr/web/product/medium/202301/451b709225ba6597b185b59e7c67c4a5.jpg",
+        title : "C93 텀블러 블루 타이드",
+        price : "￦24,000"
+      },
+
+      {
+        id : 6,
+        img: "https://crowcanyon.co.kr/web/product/medium/202301/cd7397b4e2a9770c74ec9874c5443c4a.jpg",
+        title : "C93 텀블러 핑크 선셋",
+        price : "￦24,000"
+      },
+
+
+
 
 ]
 
