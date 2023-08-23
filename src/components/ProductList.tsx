fc2af70addbf3e0bd4ffaa6c66ad3fd9276b4720
JHR <jjeong1234@gmail.com>
2023-08-23 19:45:17 +0900

    change

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Product from "./Product";
+import Product from "../pages/Product";
 
 interface ProductListProps {
   products: { name: string; description: string }[];
