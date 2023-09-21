0618a21fa871b7a09aab7c9cd0ec111d5a050247
jjeong1234 <jjeong1234@gmail.com>
2023-09-21 17:05:58 +0900

    save files

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,32 +1,55 @@
 import React, { useState } from "react";
+import { Navbar } from "../components/Navbar";
+import Footer from "../components/Footer";
+
 import "../styles/Login.css";
 
 const Login: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
-    // 로그인 API 호출 등의 로직을 추가 가능
     setIsLoggedIn(true);
   };
 
   const handleLogout = () => {
-    // 로그아웃 API 호출 등의 로직을 추가 가능
     setIsLoggedIn(false);
   };
 
   return (
-    <div className="login-container">
-      {isLoggedIn ? (
-        <div>
-          <h2>🍽 환영합니다 💖</h2>
-          <button onClick={handleLogout}>로그아웃</button>
-        </div>
-      ) : (
-        <div>
-          <h2>로그인</h2>
-          <button onClick={handleLogin}>로그인</button>
-        </div>
-      )}
+    <div>
+      <Navbar />
+      <div className="login-container">
+        {isLoggedIn ? (
+          <div>
+            <h2>🍽 환영합니다 💖</h2>
+            <button onClick={handleLogout}>로그아웃</button>
+          </div>
+        ) : (
+          <div>
+            <h2>로그인</h2>
+            <div className="input-group">
+              <input type="text" placeholder="아이디" />
+              <input type="password" placeholder="비밀번호" />
+            </div>
+            <div className="button-group">
+              <button className="login" onClick={handleLogin}>
+                로그인
+              </button>
+              <div className="joinus-mt">
+                <h5>
+                  아직 회원이 아니십니까?
+                  <br />
+                  회원가입을 하시고 다양한 혜택을 받아보세요.
+                </h5>
+              </div>
+              <a href="/joinpage">
+                <button className="joinus">Join us</button>
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+      <Footer />
     </div>
   );
 };
